baf542e7b428085ccb856cc75dcdaf9ab1f48d8f
nickriebe <47996647+nickriebe@users.noreply.github.com>
2020-09-06 08:53:26 -0500

    Create install5.sh

---
+++ install5.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Title:      ArchLinux (Reference Title File)
+# Author(s):  NickRiebe
+# URL:        https://nickriebe.com
+# GNU:        General Public License v3.0
+################################################################################
+
+# Launch into Fdisk
+fdisk /dev/mmcblk0
+d
+2
+d
+n
+1
+echo -ne '\n'
++512M
+y
+t
+1
+n
+2
+echo -ne '\n'
+echo -ne '\n'
+y
+w
