951bfb6d68fea404592583517a18f394bc82c3b2
nickriebe <47996647+nickriebe@users.noreply.github.com>
2020-09-06 08:43:02 -0500

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Title:      ArchLinux (Reference Title File)
+# Author(s):  NickRiebe
+# URL:        https://nickriebe.com
+# GNU:        General Public License v3.0
+################################################################################
+
+# Launch into Fdisk
+fdisk /dev/mmcblk0
+
+# delete shit
+d
+
+# what the shit
